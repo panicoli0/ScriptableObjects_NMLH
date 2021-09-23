e37356ee875e2e492602f2028c0a5e81dba5813c
Pablo Ariel Nicoli <pablonicoli21@gmail.com>
2021-09-23 11:37:22 +0400

    Project Setup
    main files to make the project works

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private float playerHealth = 100f;
+
+    public float PlayerHP { get => playerHealth; set => playerHealth = value; }
+
+    public void TakeDamage(float damage)
+    {
+        playerHealth -= damage;
+        //todo: create DrainDamage()
+        if (playerHealth <= 0)
+        {
+            //FindObjectOfType<EnemyAI>().IsProveked = false;
+            FindObjectOfType<GameManager>().GameOver();
+        }
+    }
+
+    private void DrainDamage()
+    {
+        //todo: continue drainig player HP like he is infected
+    }
+}
